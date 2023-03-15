06fb29ab4f6685d01956de8c9a9ee84cfe20befc
SushantChhetry <60019004+SushantChhetry@users.noreply.github.com>
2023-03-14 19:22:08 -0500

    Added initial Nav and Footer

--- cat-dog/src/pages/index.js
+++ cat-dog/src/pages/index.js
@@ -2,6 +2,8 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
+import Footer from "@/components/Footer";
+import Nav from "@/components/Nav";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +20,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
+        <Nav />
         
+        <Footer />
       </main>
     </>
   );
