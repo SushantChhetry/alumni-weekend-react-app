f8d0d9bf5e4d135107646ac3d7e67c47bd12281e
SushantChhetry <60019004+SushantChhetry@users.noreply.github.com>
2023-03-15 19:21:59 -0500

    Added countdown component

---
+++ cat-dog/src/components/Countdown.js
@@ -0,0 +1,31 @@
+import { useState, useEffect } from "react";
+
+const Countdown = ({ targetDate }) => {
+  const [countdown, setCountdown] = useState(targetDate - new Date().getTime());
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCountdown(targetDate - new Date().getTime());
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, [targetDate]);
+
+  const seconds = Math.floor((countdown / 1000) % 60);
+  const minutes = Math.floor((countdown / 1000 / 60) % 60);
+  const hours = Math.floor((countdown / 1000 / 60 / 60) % 24);
+  const days = Math.floor(countdown / 1000 / 60 / 60 / 24);
+
+  return (
+    <div>
+      {" "}
+      <div>
+        <h2>
+          {days} days {hours} hours {minutes} min {seconds} sec
+        </h2>
+      </div>
+    </div>
+  );
+};
+
+export default Countdown;
--- cat-dog/src/pages/index.js
+++ cat-dog/src/pages/index.js
@@ -4,10 +4,14 @@ import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
 import Footer from "@/components/Footer";
 import Nav from "@/components/Nav";
+import Countdown from "@/components/Countdown";
+import group from "../img/group.jpg";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const targetDate = new Date("2023-04-21T23:59:59").getTime();
+
   return (
     <>
       <Head>
@@ -22,15 +26,22 @@ export default function Home() {
       <main>
         <Nav />
         <div className="body-wrapper">
-          <div className="body"></div>
+          <div className="body">
+            <img src="../img/group.jpg" alt="home"></img>
+          </div>
+          <Countdown targetDate={targetDate} />
           <div className="button-wrapper">
             <button className="bodybtn">Events</button>
             <button className="bodybtn">RSVP</button>
             <button className="bodybtn">Donate</button>
           </div>
         </div>
-        <Footer />
+        <section>
+          <h2>Events</h2>
+          <h2>Contact</h2>
+        </section>
       </main>
+      <Footer />
     </>
   );
 }
