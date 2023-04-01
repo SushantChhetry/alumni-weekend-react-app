f859f692269ed307f437b6e6f73e6445238403d4
SushantChhetry <60019004+SushantChhetry@users.noreply.github.com>
2023-03-31 20:26:05 -0500

    Added date

--- alumni-weekend/src/pages/Event.js
+++ alumni-weekend/src/pages/Event.js
@@ -4,6 +4,7 @@ const Event = () => {
   const events = [
     {
       title: "Welcome Mixer",
+      date: "April 21",
       time: "7:00 PM",
       location: "Peerless Grille and Salon",
       lat: "33.65760295726795",
@@ -12,6 +13,7 @@ const Event = () => {
     },
     {
       title: "The Cookout",
+      date: "April 22",
       time: "3:00 - 5:00 PM",
       location: "International House",
       lat: "33.82549295263616",
@@ -20,6 +22,7 @@ const Event = () => {
     },
     {
       title: "The After Party",
+      date: "April 22",
       time: "8:00 PM",
       location: "Alumni's house",
       lat: "",
@@ -34,13 +37,15 @@ const Event = () => {
         <Image src="/img/img2.jpg" alt="event-image" width={500} height={500} />
       </div>
       <div className="event-wrapper">
-        <h1>Events</h1>
+        <h1>Evens</h1>
         {events.map((event, key) => {
           return (
             <div className="event" id={key}>
               <h3>{event.title}</h3>
               <p>{event.note ? event.note : ""}</p>
-              <p> {event.time}</p>
+              <p>
+                {event.date}, {event.time}
+              </p>
               <p>
                 <a
                   href={`https://www.google.com/maps?q=${event.lat},${event.lng}`}
